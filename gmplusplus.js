48a93daffd57d06abd9238163b376582eb5e96c7
2BandRadio <69024986+2BandRadio@users.noreply.github.com>
2020-09-11 17:13:44 +0100

    Update gmplusplus.js

--- gmplusplus.js
+++ gmplusplus.js
@@ -1,9 +1,9 @@
 (() => {
   const __GM = {
-    increment: 0,
+    increments: [0, 0],
+    onces: {},
     GM_getValue: GM_getValue,
     GM_setValue: GM_setValue,
-    GM_deleteValue: GM_deleteValue,
     GM_addValueChangeListener: GM_addValueChangeListener
   }
 
@@ -14,30 +14,31 @@
   GM_setValue = (name, value) => {
     __GM.GM_setValue(name, {
       value: value,
-      __t: __GM.increment++
+      __t: __GM.increments[0]++
     });
   }
 
-  GM_deleteValue = (name) => {
-  	if (typeof name === "string") {
-  		__GM.GM_deleteValue(name);
-  	} else {
-  		GM_listValues().forEach((valueName) => {
-  			if (name.test(valueName)) {
-  				__GM.GM_deleteValue(valueName);
-  			}
-  		});
-  	}
-  }
-
-  GM_addValueChangeListener = (name, callback, onDelete = true) => { 
-    return __GM.GM_addValueChangeListener(name, (name, pre, cur, remote) => {
+  GM_addValueChangeListener = (name, callback, once = false, onDelete = true) => {
+    const listenerId = `${__GM.increments[1]++}`;
+    
+    const listener = __GM.GM_addValueChangeListener(name, (name, pre, cur, remote) => {
       pre = pre || { value: undefined };
       cur = cur || { value: undefined };
       const deleted = !GM_listValues().includes(name);
       
-      if (!(!onDelete && deleted) && (pre.value !== cur.value))
+      if (!(!onDelete && deleted) && (pre.value !== cur.value)) {
+        if (once)
+          GM_removeValueChangeListener(__GM.onces[listenerId]);
+        
         callback(name, pre.value, cur.value, remote, deleted);
+      }
     });
+    
+    if (once)
+      __GM.onces[listenerId] = listener;
+    
+    return listener;
   }
+
+  unsafeWindow.GMplusplus = "2019111713";
 })();
